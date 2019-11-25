87679509b09d871a22afbff3ea85b810016231a5
Daniel <a20110645@pucp.pe>
2019-11-24 21:55:51 -0500

    Add permission request and working media player

--- code/app/src/main/java/com/example/videoarch/MainActivity.kt
+++ code/app/src/main/java/com/example/videoarch/MainActivity.kt
@@ -1,43 +1,106 @@
 package com.example.videoarch
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
+import android.widget.Button
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var videoView: VideoView
+    private lateinit var playButton: Button
+    private lateinit var pauseButton: Button
+    private lateinit var stopButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mediaSession = MediaSessionCompat(this, "Session_Tag").apply {
-
-            //Por defecto se maneja tanto los media buttons como los transport controls
-            //Tambien por defecto los convierte en transport control
-
-            //Por ser video se le indica que en background no va a recibir ningun media button
-            setMediaButtonReceiver(null)
-
-            // Se le indica al controller que media buttons van a ser validos
-            val stateBuilder = PlaybackStateCompat.Builder()
-                .setActions(
-                    PlaybackStateCompat.ACTION_PLAY or
-                            PlaybackStateCompat.ACTION_PLAY_PAUSE or
-                            PlaybackStateCompat.ACTION_STOP
+        //Pedir permiso
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.READ_CONTACTS
                 )
-            setPlaybackState(stateBuilder.build())
-
-            // Se le asigna los callbacks para manejar el media session
-            setCallback(MySessionCallbacks())
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    MY_PERMISSIONS_REQUEST_READ_STORAGE
+                )
+            }
+        } else {
+            setupVideoView()
         }
 
-        MediaControllerCompat(this, mediaSession).also { mediaControllerCompat ->
-            MediaControllerCompat.setMediaController(this, mediaControllerCompat)
+        playButton = findViewById(R.id.play)
+        playButton.setOnClickListener {
+            mediaController.transportControls.play()
+        }
+        pauseButton = findViewById(R.id.pause)
+        pauseButton.setOnClickListener {
+            mediaController.transportControls.pause()
+        }
+        stopButton = findViewById(R.id.stop)
+        stopButton.setOnClickListener {
+            mediaController.transportControls.stop()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_READ_STORAGE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    setupVideoView()
+                }
+                return
+            }
+        }
+    }
+
+
+    private fun setupVideoView() {
+        videoView = findViewById(R.id.video)
+        videoView.setVideoPath("/storage/emulated/0/Movies/Smash_Mouth_-_All_Star_Official_Music_Video.mp4")
+        videoView.setOnPreparedListener {
+            mediaSession = MediaSessionCompat(this, "Session_Tag").apply {
+
+                //Por defecto se maneja tanto los media buttons como los transport controls
+                //Tambien por defecto los convierte en transport control
+
+                //Por ser video se le indica que en background no va a recibir ningun media button
+                setMediaButtonReceiver(null)
+
+                // Se le indica al controller que media buttons van a ser validos
+                val stateBuilder = PlaybackStateCompat.Builder()
+                    .setActions(
+                        PlaybackStateCompat.ACTION_PLAY or
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE or
+                                PlaybackStateCompat.ACTION_STOP
+                    )
+                setPlaybackState(stateBuilder.build())
+
+                // Se le asigna los callbacks para manejar el media session
+                setCallback(MySessionCallbacks(it))
+            }
+
+            MediaControllerCompat(this, mediaSession).also { mediaControllerCompat ->
+                MediaControllerCompat.setMediaController(this, mediaControllerCompat)
+            }
         }
     }
 
@@ -53,6 +116,10 @@ class MainActivity : AppCompatActivity() {
         if (android.os.Build.VERSION.SDK_INT > 23) {
             this.mediaController.transportControls.stop()
         }
+    }
+
+    companion object {
+        const val MY_PERMISSIONS_REQUEST_READ_STORAGE = 25
     }
 
 }
--- code/app/src/main/java/com/example/videoarch/MySessionCallbacks.kt
+++ code/app/src/main/java/com/example/videoarch/MySessionCallbacks.kt
@@ -1,19 +1,23 @@
 package com.example.videoarch
 
+import android.media.MediaPlayer
 import android.support.v4.media.session.MediaSessionCompat
 
-class MySessionCallbacks : MediaSessionCompat.Callback() {
+class MySessionCallbacks(var player: MediaPlayer) : MediaSessionCompat.Callback() {
 
     override fun onPlay() {
         super.onPlay()
+        player.start()
     }
 
     override fun onStop() {
         super.onPlay()
+        player.stop()
     }
 
     override fun onPause() {
         super.onPlay()
+        player.pause()
     }
 
 }
