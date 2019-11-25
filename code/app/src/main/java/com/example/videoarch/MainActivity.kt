9c838188f0bad2cd8723b1666a8235db3aff3681
Daniel <a20110645@pucp.pe>
2019-11-24 20:10:39 -0500

    Add media session stuff and add buttons for example

--- code/app/src/main/java/com/example/videoarch/MainActivity.kt
+++ code/app/src/main/java/com/example/videoarch/MainActivity.kt
@@ -1,12 +1,58 @@
 package com.example.videoarch
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaSession: MediaSessionCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaSession = MediaSessionCompat(this, "Session_Tag").apply {
+
+            //Por defecto se maneja tanto los media buttons como los transport controls
+            //Tambien por defecto los convierte en transport control
+
+            //Por ser video se le indica que en background no va a recibir ningun media button
+            setMediaButtonReceiver(null)
+
+            // Se le indica al controller que media buttons van a ser validos
+            val stateBuilder = PlaybackStateCompat.Builder()
+                .setActions(
+                    PlaybackStateCompat.ACTION_PLAY or
+                            PlaybackStateCompat.ACTION_PLAY_PAUSE or
+                            PlaybackStateCompat.ACTION_STOP
+                )
+            setPlaybackState(stateBuilder.build())
+
+            // Se le asigna los callbacks para manejar el media session
+            setCallback(MySessionCallbacks())
+        }
+
+        MediaControllerCompat(this, mediaSession).also { mediaControllerCompat ->
+            MediaControllerCompat.setMediaController(this, mediaControllerCompat)
+        }
     }
+
+    override fun onPause() {
+        super.onPause()
+        if (android.os.Build.VERSION.SDK_INT <= 23) {
+            this.mediaController.transportControls.stop()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (android.os.Build.VERSION.SDK_INT > 23) {
+            this.mediaController.transportControls.stop()
+        }
+    }
+
 }
---
+++ code/app/src/main/java/com/example/videoarch/MySessionCallbacks.kt
@@ -0,0 +1,19 @@
+package com.example.videoarch
+
+import android.support.v4.media.session.MediaSessionCompat
+
+class MySessionCallbacks : MediaSessionCompat.Callback() {
+
+    override fun onPlay() {
+        super.onPlay()
+    }
+
+    override fun onStop() {
+        super.onPlay()
+    }
+
+    override fun onPause() {
+        super.onPlay()
+    }
+
+}
